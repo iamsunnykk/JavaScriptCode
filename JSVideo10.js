6282da8ce9507b2b220096bbfc702a0f63973687
Sunil Khanadale <sunilkhanadalee@gmail.com>
2025-12-09 19:32:08 +0530

    Video10
    Code

---
+++ JSVideo10.js
@@ -0,0 +1,18 @@
+//while Loop
+let input = 5;
+input = Number(input);
+let i = 0;
+
+while (i < input) {
+    console.log(i);  // print the current value of i
+    i++;  // increment i by 1
+}
+
+//Example without i++
+// let input1 = 5;
+// input = Number(input);
+// let i1 = 0;
+
+// while (i1 < input1) {
+//     console.log(i1);  // print the current value of i
+// }
